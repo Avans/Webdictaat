5320e0ffbc7d9cf66bea6db9f8745d3156ded526
Paul Wagener <mail@paulwagener.nl>
2015-07-29 13:01:37 +0200

    Test Advans

---
+++ app/app.js
@@ -0,0 +1,6 @@
+
+var app = angular.module('webs1', ['PointyPony', 'ui.router']);
+
+app.constant('appConfig', {
+  courseToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.IldFQlMgdGVzdCI.LmMGE0RDudXtO47AmsW3OPhT9aXc0mdBX5JhC7Bj1IQ",
+});
